35f99370a97f871df43c87f6ec4eb966cc7cd3a8
aidendangelo <31748446+aidendangelo@users.noreply.github.com>
2020-11-09 13:31:10 -0500

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,17 @@
+// Import react
 import React from 'react';
 
+// Declares a constant for the Nagivation Component
+// Accepts route function, and signed in function
 const Navigation = ({ onRouteChange, isSignedIn }) => {
+    // If they are signed in, returns a signout button
     if (isSignedIn) {
       return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
           <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
       );
+    // If they are not signed in, returns a sign in/ register button
     } else {
       return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
